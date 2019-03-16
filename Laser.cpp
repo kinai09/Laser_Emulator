1f5a21b2898164f9a2a7b240db12eea41215a6ef
kinai09 <48614420+kinai09@users.noreply.github.com>
2019-03-16 18:51:02 +0800

    Checked validity of command and repond with error

--- Laser.cpp
+++ Laser.cpp
@@ -6,11 +6,13 @@ Laser::Laser()
 {
 	m_request = "";
 	m_response = "";
+	
 }
 
 Laser::~Laser()
 {
 }
+
 
 
 void Laser::setRequest(string request)
@@ -21,7 +23,13 @@ void Laser::setRequest(string request)
 	size_t pipePosition = request.find("|");
 	if ( pipePosition != string::npos)
 		m_command = request.substr(0, pipePosition);
+	
 	m_response = m_command;
+	
+	if (!isValidCommand())
+		m_response = m_response +  COMMAND_ERROR;
+	
+	
 }
 
 string Laser::getResponse()
@@ -35,4 +43,26 @@ void Laser::processRequest()
 
 void Laser::setResponse(string response)
 {
+}
+
+bool Laser::isValidCommand()
+{
+	if (!m_command.compare(STR))
+		return true;
+	if (!m_command.compare(STP))
+		return true;
+	if (!m_command.compare(ST))
+		return true;
+	if (!m_command.compare(KAL))
+		return true;
+	if (!m_command.compare(PW_GET))
+		return true;
+	if (!m_command.compare(PW_SET))
+		return true;
+	if (!m_command.compare(ESM))
+		return true;
+	if (!m_command.compare(DSM))
+		return true;
+	
+	return false;
 }
--- Laser.hpp
+++ Laser.hpp
@@ -2,6 +2,20 @@
 #define LASER_H
 
 #include <string>
+#include <list>
+
+#define STR "STR"
+#define STP (string)"STP"
+#define ST  (string)"ST?"
+#define KAL (string)"KAL"
+#define PW_GET (string)"PW?"
+#define PW_SET (string)"PW="
+#define ESM (string)"ESM"
+#define DSM (string)"DSM"
+
+
+#define COMMAND_ERROR "UK!"
+
 
 
 class Laser
@@ -18,10 +32,13 @@ class Laser
 		void setResponse(std::string response);
 		
 	private:
+	
+		bool isValidCommand();
+		
 		std::string m_request;
 		std::string m_response;
-		
 		std::string m_command;
+
 };
 
 #endif // LASER_H
