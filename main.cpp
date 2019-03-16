588b4973f49166da55ee8ef03deb67c6f25f02ca
kinai09 <48614420+kinai09@users.noreply.github.com>
2019-03-16 18:10:21 +0800

    Added termination of application when received SIGTERM

--- main.cpp
+++ main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
-
+#include <cstring>
 
 using namespace std;
 
+volatile sig_atomic_t applicationExit = false;
+
+void terminateProgram(int signum)
+{
+	applicationExit = true;
+}
+
 int main()
 {
+	struct sigaction action;
+	memset(&action, 0, sizeof(struct sigaction));
+	action.sa_handler = terminateProgram;
+	sigaction(SIGTERM, &action, NULL);
+	
+	while(!applicationExit)
+	{
+		
+	}
+	
+	cout << "Application Terminated";
+	
+	
     return 0;
 }
